70adb72493470dac9a324006d05c152e9c177a45
Chanket <Chanket@qq.com>
2018-11-06 22:37:41 +0800

    完善注释

--- Searcher/Searcher.cs
+++ Searcher/Searcher.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace MagnetX.Searcher
 {
+    /// <summary>
+    /// 搜索结果。
+    /// </summary>
     public class Result
     {
         public string Name { get; set; }
@@ -14,6 +17,9 @@ namespace MagnetX.Searcher
         public string From { get; set; }
     }
 
+    /// <summary>
+    /// <see cref="Searcher"/>的状态。
+    /// </summary>
     public enum TestResults
     {
         OK,
@@ -23,18 +29,30 @@ namespace MagnetX.Searcher
         UnknownError,
     }
 
+    /// <summary>
+    /// 标记<see cref="Searcher"/>为启用状态的属性。
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class SearcherEnabledAttribute : System.Attribute
     {
 
     }
 
+    /// <summary>
+    /// <see cref="Searcher"/>返回搜索结果的事件委托。
+    /// </summary>
+    /// <param name="sender">返回搜索结果的<see cref="Searcher"/>对象。</param>
+    /// <param name="results">搜索结果的列表。</param>
+    /// <returns>指示是否继续搜索。</returns>
+    delegate bool SearcherResultsDelegate(Searcher sender, List<Result> results);
+
+    /// <summary>
+    /// 数据源的抽象类。
+    /// </summary>
     abstract class Searcher
     {
-        public delegate bool SearcherResultsDelegate(Searcher sender, List<Result> results);
         public SearcherResultsDelegate OnResults = null;
 
-        public bool Break { get; set; } = false;
         public object Tag { get; set; } = null;
         public abstract string Name { get; }
         public abstract void SearchAsync(string word);
