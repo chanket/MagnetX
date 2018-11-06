88b1a7dca5e7b338109642a2dd4fbf6037061600
Chanket <Chanket@qq.com>
2018-11-06 23:54:11 +0800

    添加源bturl.cc

---
+++ Searcher/WebSearcher/BturlWebSearcher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MagnetX.Searcher.WebSearcher
+{
+    [SearcherEnabled]
+    class BturlWebSearcher : WebSearcher
+    {
+        public override string Name => "bturl.cc";
+
+        protected override async Task<string> GetURL(string word, int page)
+        {
+            string name = Uri.EscapeUriString(word);
+            return "https://www.bturl.cc/search/" + name + "_click_" + page + ".html";
+        }
+
+        protected override IEnumerable<string> PrepareParts(string content)
+        {
+            string[] parts = content.Split(new string[] { "<h3 class=\"T1\">" }, StringSplitOptions.None);
+            for (int i = 1; i < parts.Length; i++)
+            {
+                yield return parts[i];
+            }
+
+        }
+        protected Regex regName = new Regex("<div class=\"item-list\">(.+)(</div>|\n)", RegexOptions.Compiled);
+        protected Regex regMagnet = new Regex("href=\"/(.+?)\\.html", RegexOptions.Compiled);
+        protected Regex regSize = new Regex("文件大小.+?>(.+?)</span>", RegexOptions.Compiled);
+
+        protected override async Task<Result> ReadPart(string part)
+        {
+            Result result = new Result() { From = Name, };
+            try
+            {
+                var matchName = regName.Match(part);
+                var matchMagnet = regMagnet.Match(part);
+                var matchSize = regSize.Match(part);
+                if (!matchName.Success || !matchMagnet.Success || !matchSize.Success) return null;
+
+                result.Name = matchName.Groups[1].Value;
+                result.Magnet = "magnet:?xt=urn:btih:" + matchMagnet.Groups[1].Value;
+                result.Size = matchSize.Groups[1].Value;
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
